adc1acafba5faeebd91f87384145fdb8124821d7
darwady2 <darwady2@gmail.com>
2017-12-22 19:33:00 -0800

    finished estimator tutorial

--- tfTesting.py
+++ tfTesting.py
@@ -27,7 +27,19 @@ b = tf.Variable([-.3], dtype=tf.float32)
 x = tf.placeholder(tf.float32)
 linear_model = W*x + b
 
-init = tf.global_variables_initializer()
-sess.run(init)
+init = tf.global_variables_initializer() # This is needed to initialize the variables, before you can run the program.
 
 #print(sess.run(linear_model, {x: [1, 2, 3, 4]})) # Outputs [ 0.  0.30000001  0.60000002  0.90000004]
+
+y = tf.placeholder(tf.float32) #This is a placeholder for the training data. We'll input what we want the values to be.
+squared_deltas = tf.square(linear_model - y) #This is the squared difference between the output of our linear model and our training data points.
+loss = tf.reduce_sum(squared_deltas) #This sums up the squared deltas into one scalar -  a coefficient that gives an indicator for how far off our model is.
+
+optimizer = tf.train.GradientDescentOptimizer(0.01) # This creates an optimizer, which we'll use to adjust the coefficients until they become correct.
+train = optimizer.minimize(loss) #This tells our optimizer to minimize the value of 'loss'.
+
+sess.run(init) # Reinitializes the variables.
+for i in range(1000): #Runs our optimizer 1000 times over the values of x vs. the values of y, and adjusts the coefficient until the input of x will result in the output of y.
+    sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+
+print(sess.run([W, b])) # Output: W = -1, b = 1.
